f82e64a83e18e7c0f6836b71c7a841b61a5458ab
e-barton222 <ethan.barton@g.austincc.edu>
2025-10-18 23:16:46 -0500

    Update question2.cpp

--- src/question_2/question2.cpp
+++ src/question_2/question2.cpp
@@ -1,6 +1,24 @@
+#include <iostream>
 #include "question2.h"
 
-bool test_config()
-{
-    return true;
+double get_fahrenheit(double celsius) {
+    return (9.0 / 5.0) * celsius + 32;
+}
+
+int main() {
+    double celsius;
+    char cont = 'y';
+
+    while (cont == 'y' || cont == 'Y') {
+        std::cout << "Enter temperature in Celsius: ";
+        std::cin >> celsius;
+
+        double fahrenheit = get_fahrenheit(celsius);
+        std::cout << "Fahrenheit: " << fahrenheit << "\n";
+
+        std::cout << "Do you want to convert another? (y/n): ";
+        std::cin >> cont;
+    }
+
+    return 0;
 }
